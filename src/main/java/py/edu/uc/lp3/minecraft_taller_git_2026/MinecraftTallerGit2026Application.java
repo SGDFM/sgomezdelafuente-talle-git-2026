e7969baa86fcde9b3cbfafb52ad75d05095a260c
SGDFM <sebastianmacchi5@gmail.com>
2026-09-16 14:36:54 -0300

    chore: esqueleto Spring Boot API REST desde start.spring.io

---
+++ src/main/java/py/edu/uc/lp3/minecraft_taller_git_2026/MinecraftTallerGit2026Application.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.minecraft_taller_git_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftTallerGit2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftTallerGit2026Application.class, args);
+	}
+
+}
